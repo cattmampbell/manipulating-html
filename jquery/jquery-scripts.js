6919bc9c7e6014a477d1b16d92abeea3866281f8
Matthew Campbell <cattmampbell@gmail.com>
2018-10-03 01:51:10 -0400

    Added interactivity to jquery-scripts.js file via jQuery.

---
+++ jquery/jquery-scripts.js
@@ -0,0 +1,45 @@
+// .ready() event handler method on document
+$(document).ready(() => {
+    // Creates <button>, appends to #row2
+    $('#row2').append('<button class="btn btn-lg btn-secondary col-6 py-2 mb-4" id="alertBtn">Click Me</button>');
+
+    // Creates <div id="divBox">, appends to #row4 and styles it
+    $('#row4').append('<div id="divBox" class="my-4"></div>');
+    $('#divBox').css({
+        'background-color': '#3b4249',
+        'width': '15rem',
+        'height': '15rem',
+        'border-radius': '0.5rem'
+    });
+
+    // .on('click') event handler method on #textBtn
+    $('#textBtn').on('click', () => {
+        alert(`"${$('#textArea').val()}"`);
+        $('#textArea').val(''); // Clear!
+    })
+
+    const myColors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#17a2b8', '#f8f9fa'];
+
+    // Randomly selects an index from myColors, assigns value to myRandomColor
+    const getRandomColor = () => {
+        const myRandomColor = myColors[Math.floor(Math.random() * myColors.length)];
+        return myRandomColor;
+    }
+
+    // .mouseenter(event) event handler method on #divBox
+    $('#divBox').mouseenter ((event) => {
+        const thisRandomColor = getRandomColor();
+        $(event.target).css('background-color', thisRandomColor);
+    })
+
+    // .mouseleave(event) event handler method on #divBox
+    $('#divBox').mouseleave ((event) => {
+        $(event.target).css('background-color', '#3b4249');
+    })
+
+    // .click(event) event handler method on #pText
+    $('#pText').click ((event) => {
+        const thisRandomColor = getRandomColor();
+        $(event.target).css('color', thisRandomColor);
+    })
+})
